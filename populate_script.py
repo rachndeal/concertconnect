a3733f310f775cf8b01476bc263170fdb169a041
Adrian0git <123101782+Adrian0git@users.noreply.github.com>
2023-03-23 11:31:08 +0000

    Update populate_script.py

--- populate_script.py
+++ populate_script.py
@@ -8,48 +8,47 @@ from concertconnect_app.models import Category, Page
 
 
 def populate():
-
       ao_arena = [
        {'title': 'AO Arena',
         'url': 'https://www.ao-arena.com',
         'views': 400}]
 
-   first_direct_arena = [
+      first_direct_arena = [
        {'title': 'First Direct Arena',
         'url': 'https://www.firstdirectarena.com',
         'views': 350}]
 
-   motorpoint_arena = [
+      motorpoint_arena = [
        {'title': 'Motorpoint Arena',
         'url': 'https://www.motorpointarenanottingham.com',
         'views': 300}]
 
-   o2_academy = [
+      o2_academy = [
        {'title': 'O2 Academy',
         'url': 'https://www.academymusicgroup.com/o2academybirmingham',
         'views': 450}]
 
-   ovo_hydro_arena = [
+      ovo_hydro_arena = [
        {'title': 'OVO Hydro Arena',
         'url': 'https://www.thessehydro.com',
         'views': 500}]
 
-   pj_live_arena = [
+      pj_live_arena = [
        {'title': 'P&J Live Arena',
         'url': 'https://www.pandjlive.com',
         'views': 200}]
 
-   resorts_world_arena = [
+      resorts_world_arena = [
        {'title': 'Resorts World Arena',
         'url': 'https://www.resortsworldarena.co.uk',
         'views': 550}]
 
-   utilita_arena = [
+      utilita_arena = [
        {'title': 'Utilita Arena',
         'url': 'https://www.utilitaarena.co.uk',
         'views': 600}]
 
-   cats = {'AO Arena': {'pages': ao_arena, 'views': 400, 'likes': 200},
+      cats = {'AO Arena': {'pages': ao_arena, 'views': 400, 'likes': 200},
              'First Direct Arena': {'pages': first_direct_arena, 'views': 350, 'likes': 175},
              'Motorpoint Arena': {'pages': motorpoint_arena, 'views': 300, 'likes': 150},
              'O2 Academy': {'pages': o2_academy, 'views': 450, 'likes': 225},
@@ -58,20 +57,13 @@ def populate():
              'Resorts World Arena': {'pages': resorts_world_arena, 'views': 550, 'likes': 275},
              'Utilita Arena': {'pages': utilita_arena, 'views': 600, 'likes': 300}
              }
-
-   
-    # If you want to add more categories or pages,
-    # add them to the dictionaries above.
-
-    # The code below goes through the cats dictionary, then adds each category,
-    # and then adds all the associated pages for that category.
-    for cat, cat_data in cats.items():
-        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
-        for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'], views=p['views'] )
- 
-    # Print out the categories we have added.
-    for c in Category.objects.all():
+        
+      for cat, cat_data in cats.items():
+              c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
+              for p in cat_data['pages']:
+                  add_page(c, p['title'], p['url'], views=p['views'] )
+                  
+      for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
 
